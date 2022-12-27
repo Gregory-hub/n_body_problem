e61af974c87bfa5e9342b0aa2a84afe2a2f16f95
Gregory-hub <novikov.gri@gmail.com>
2022-12-27 22:04:04 +0300

    videos

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from scene import NBodySystemScene
 
 
 if __name__ == "__main__":
-    scene = NBodySystemScene(engine, step_size=10, update_period=1)
+    scene = NBodySystemScene(engine, step_size=0.2, update_period=1)
     engine.setup(scene)
     engine.run()
